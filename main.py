5ed7073231806fc520613fe722008f2981ee9f67
Shoham Houta <shohamho@gmail.com>
2022-07-07 03:43:17 +0300

    *

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sqlite3 
 
-con = sqlite3.connect(':memory:')
+con = sqlite3.connect('contacts.db')
 cur = con.cursor()
+
 
 
 def main():
     pass
+
+
 if __name__ == '__main__':
     main()
