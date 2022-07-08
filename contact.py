be5990229cfcd785395b1961c6d4bb937ce8112d
Shoham Houta <shohamho@gmail.com>
2022-07-08 18:49:21 +0300

    **

--- contact.py
+++ contact.py
@@ -1,58 +1,67 @@
 import phonenumbers
+import pandas as pd
 import re
 
+
 class InvalidEmail(Exception):
-    def __init__(self,email) -> None:
+    def __init__(self, email) -> None:
         self.email = email
         self.message = f"{self.email} Is not a valid Email!"
         super().__init__(self.message)
 
+
 class InvalidPhone(Exception):
-    def __init__(self,phone) -> None:
+    def __init__(self, phone) -> None:
         self.phone = phone
         self.message = f"{self.phone} Is Not a valid phone!"
         super().__init__(self.message)
 
+
 class InfoChecks():
 
+
     def email_check(email):
-        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$' 
-        tmp = re.search(regex,email)
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        tmp = re.search(regex, email)
         if tmp:
             return True
         else:
             raise InvalidEmail(email)
 
     def phone_check(phone):
-        number = phonenumbers.parse(phone)    
-        if ~phonenumbers.is_possible_number(number):
+        Country_Codes = pd.read_csv("PhonesCountryCodes.csv")
+        Codex = Country_Codes["COUNTRY CODE"].to_list()
+        for code in Codex:
+            if code in phone: 
+                number = phonenumbers.parse(phone)
+        if not phonenumbers.is_possible_number(number):
             raise InvalidPhone(number)
+
+
 class Contact:
-    def __init__(self,name,last,address,phone,email) -> None:
-        self.name = name
-        self.last = last
-        self.address = address
-        self.phone = phone
-        self.email = email
-    
+    def __init__(self, name, last, address, phone, email) -> None:
+        self.Name = name
+        self.Last = last
+        self.Address = address
+        self.Phone = phone
+        self.Email = email
+
     @property
     def email(self):
-        return self.email
+        return self.Email
 
     @property
     def address(self):
-        return self.address
-    
+        return self.Address
+
     @property
     def phone(self):
-        return self.phone
+        return self.Phone
 
     @property
     def fullname(self):
-        return f"{self.name} {self.last}"
+        return f"{self.Name} {self.Last}"
 
     def __repr__(self) -> str:
-        return f"""Full Name:{self.name} {self.last})
-        Address:{self.address}
-        Phone:{self.phone}
-        E-mail:{self.email}"""
+        return f"({self.Name} {self.Last},{self.Address},{self.Phone},{self.Email})"
+                       
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import email
-import sqlite3
+import sqlite3,pandas as pd
 from contact import *
 
 con = sqlite3.connect('contacts.db')
@@ -7,18 +6,37 @@ cur = con.cursor()
 
 
 
-def add_contact():
-    print("Add New Contact:")
+def create_contact():
+    """
+    Create new instence of Contact
+    """
+    Country_Codes = pd.read_csv("PhonesCountryCodes.csv")
+    Codex = Country_Codes["COUNTRY CODE"].to_list()
+    Countries = Country_Codes["COUNTRY"].to_list()
+    region_list = list(zip(Countries,Codex))
+    
+    print("New Entry:")
+    
     name = input("Enter a name:")
     lname = input("Enter last name:")
     address = input("Enter an address:")
+    
     while True:
         try:    
-            phone = input("Enter phone number:")
+            for index,(c, code) in enumerate(region_list):
+                print(f"({index}) {c}:{code}")
+    
+            choice = int(input("Chose Your country Code:"))
+            country_code = '+'+Codex[choice] 
+            phone = country_code + input("Enter phone number:")
+    
             InfoChecks.phone_check(phone)
+    
             email = input("Enter email address:")
+    
             InfoChecks.email_check(email)
             break
+    
         except InvalidPhone:
             print("Invalid phone number!")
         except InvalidEmail:
@@ -28,7 +46,7 @@ def add_contact():
     return contact
 
 def main():
-    c = add_contact()
+    c = create_contact()
     print(c)
     
 if __name__ == '__main__':
